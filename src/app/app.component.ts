99f507306070cdbe126338715c24ccd63a3d6d29
ROTY Jeremy <rotyjeremy@gmail.com>
2024-03-24 11:10:47 +0100

    update routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatSelectModule} from "@angular/material/select";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatTableModule} from "@angular/material/table";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatListModule} from "@angular/material/list";
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatRadioModule} from "@angular/material/radio";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatDateFormats, MatNativeDateModule} from "@angular/material/core";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { TeamsComponent } from './pages/teams/teams.component';
+import { LegalComponent } from './pages/legal/legal.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'pages/contact', component: ContactComponent},
+    { path: 'pages/pricing', component: PricingComponent },
+    { path: 'pages/technology', component: TechnologyComponent },
+    { path: 'pages/teams', component: TeamsComponent },
+    { path: 'pages/legal', component: LegalComponent },
+    { path: 'pages/privacy', component: PrivacyComponent },
+    {path: '**', component: NotFoundComponent}
+];
